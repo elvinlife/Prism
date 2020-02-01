c843ee3998c5527cf8dba43a1da4470f6c545c28
sie2 <sie2@illinois.edu>
2020-01-31 22:59:10 -0600

    Implemented merkle and transaction

--- src/crypto/merkle.rs
+++ src/crypto/merkle.rs
@@ -1,29 +1,124 @@
 use super::hash::{Hashable, H256};
+use std::vec::Vec;
 
 /// A Merkle tree.
 #[derive(Debug, Default)]
 pub struct MerkleTree {
+    tree: Vec<H256>,
+    valid: Vec<bool>,
+    sz: usize,
 }
 
 impl MerkleTree {
     pub fn new<T>(data: &[T]) -> Self where T: Hashable, {
-        unimplemented!()
+        let mut _sz = 1;
+        while _sz < data.len(){
+            _sz = _sz << 1;
+        }
+
+        let mut _tree: Vec<H256> = Vec::<H256>::new();
+        _tree.resize(2*_sz-1,Default::default());
+        let mut _valid: Vec<bool> = Vec::<bool>::new();
+        _valid.resize(2*_sz-1,false);
+
+        for i in 0..data.len(){
+            _tree[i+_sz-1] = data[i].hash();
+            _valid[i+_sz-1] = true;
+        }
+
+        let save_sz = _sz;
+        while _sz > 1{
+            let mut i = 0;
+            while i < _sz {
+                let l_idx = _sz - 1 + i;
+                let r_idx = l_idx + 1;
+                let p_idx = (l_idx - 1) >> 1;
+
+                let mut buf : Vec<u8> = Vec::<u8>::new();
+
+                if !_valid[l_idx]{
+                    break;
+                }
+                else if _valid[l_idx] && !_valid[r_idx]{
+                    _tree[r_idx] = _tree[l_idx];
+                    buf.extend_from_slice(_tree[l_idx].as_ref()); 
+                    buf.extend_from_slice(_tree[r_idx].as_ref());
+                    _tree[p_idx] = ring::digest::digest(&ring::digest::SHA256, &buf).into();
+                    _valid[p_idx] = true;
+                }
+                else{
+                    buf.extend_from_slice(_tree[l_idx].as_ref()); 
+                    buf.extend_from_slice(_tree[r_idx].as_ref());
+                    _tree[p_idx] = ring::digest::digest(&ring::digest::SHA256, &buf).into();
+                    _valid[p_idx] = true;
+                } 
+
+                i += 2;
+            }
+            _sz  = _sz >> 1;
+        }
+
+        MerkleTree{
+            tree: _tree,
+            valid: _valid,
+            sz: save_sz,
+        }
+
     }
 
     pub fn root(&self) -> H256 {
-        unimplemented!()
+        self.tree[0]
     }
 
     /// Returns the Merkle Proof of data at index i
     pub fn proof(&self, index: usize) -> Vec<H256> {
-        unimplemented!()
+        let mut proof : Vec<H256> = Vec::<H256>::new();
+        let mut idx = self.sz - 1 + index;
+        if idx < 2*self.sz - 1 && self.valid[idx]{
+            while idx > 0{
+                let p_idx = (idx - 1) >> 1;
+                let s_idx = if idx % 2 == 1{
+                    idx + 1
+                }
+                else{
+                    idx - 1
+                };
+                proof.push(self.tree[s_idx]);
+                idx = p_idx;
+            }
+        } 
+        proof 
     }
 }
 
 /// Verify that the datum hash with a vector of proofs will produce the Merkle root. Also need the
 /// index of datum and `leaf_size`, the total number of leaves.
 pub fn verify(root: &H256, datum: &H256, proof: &[H256], index: usize, leaf_size: usize) -> bool {
-    unimplemented!()
+    if index >= leaf_size{
+        false
+    }
+    else{
+        let mut idx = index;
+        let mut curr : H256 = *datum;
+
+        for hash in proof{
+            let mut buf : Vec<u8> = Vec::<u8>::new();
+            if idx % 2 == 0{
+                buf.extend_from_slice(curr.as_ref());
+                buf.extend_from_slice(hash.as_ref());
+                curr = ring::digest::digest(&ring::digest::SHA256,&buf).into(); 
+            }
+            else{
+                buf.extend_from_slice(hash.as_ref());
+                buf.extend_from_slice(curr.as_ref());
+                curr = ring::digest::digest(&ring::digest::SHA256,&buf).into();
+            }
+            idx = idx >> 1;
+        }
+    
+        *root == curr 
+    }
+    
 }
 
 #[cfg(test)]
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,18 +1,24 @@
 use serde::{Serialize,Deserialize};
-use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters};
+use ring::signature::{Ed25519KeyPair, Signature, KeyPair, VerificationAlgorithm, EdDSAParameters, UnparsedPublicKey, ED25519};
+use rand::{Rng, distributions::Alphanumeric};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Transaction {
+    input: String,
+    output: String,
 }
 
 /// Create digital signature of a transaction
 pub fn sign(t: &Transaction, key: &Ed25519KeyPair) -> Signature {
-    unimplemented!()
+    let t_bytes = bincode::serialize(t).unwrap();
+    key.sign(&t_bytes)  
 }
 
 /// Verify digital signature of a transaction, using public key instead of secret key
 pub fn verify(t: &Transaction, public_key: &<Ed25519KeyPair as KeyPair>::PublicKey, signature: &Signature) -> bool {
-    unimplemented!()
+    let t_bytes = bincode::serialize(t).unwrap();
+    let public_key = UnparsedPublicKey::new(&ED25519, public_key);
+    public_key.verify(&t_bytes, signature.as_ref()).is_ok()
 }
 
 #[cfg(any(test, test_utilities))]
@@ -21,8 +27,9 @@ mod tests {
     use crate::crypto::key_pair;
 
     pub fn generate_random_transaction() -> Transaction {
-        Default::default()
-        //unimplemented!()
+        let rand_input = rand::thread_rng().sample_iter(&Alphanumeric).take(10).collect::<String>();
+        let rand_output = rand::thread_rng().sample_iter(&Alphanumeric).take(10).collect::<String>();
+        Transaction { input : rand_input, output: rand_output }
     }
 
     #[test]
