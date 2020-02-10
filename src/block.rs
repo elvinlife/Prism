c0f662f5ffefe958816ef4a2e442edd7dccdb285
sie2 <sie2@illinois.edu>
2020-02-09 21:58:00 -0600

    Initial Block struct in block.rs

--- src/block.rs
+++ src/block.rs
@@ -1,8 +1,26 @@
 use serde::{Serialize, Deserialize};
 use crate::crypto::hash::{H256, Hashable};
+use chrono::{DateTime, Utc};
+use crate::transaction::{Transaction};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Header{
+    parent: H256,
+    nonce: u32,
+    difficulty: H256,
+    timestamp: DateTime<Utc>,
+    merkle_root: H256,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Content{
+    transactions: Vec<Transaction>,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Block {
+    header: Header,
+    content: Content,
 }
 
 impl Hashable for Block {
